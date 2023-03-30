ad0bb375fd6e6f71b7774680c7fbc9cd7c42ef16
Andreoly Jesus <andreolyjesus@hotmail.com>
2023-03-30 19:48:30 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -25,5 +23,36 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("BootCamp Java Develope");
+        bootCamp.setDescricao("Descrição BootCamp Java Develop");
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev devAndreoly = new Dev();
+        devAndreoly.setNome("Andreoly");
+        devAndreoly.escreverBootcamp(bootCamp);
+        System.out.println("Conteudos Inscritos Andreoly:"+devAndreoly.getConteudosInscristos());
+        devAndreoly.progredir();
+        devAndreoly.progredir();
+        devAndreoly.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Andreoly:"+devAndreoly.getConteudosInscristos());
+        System.out.println("Conteudos Concluidos Andreoly:"+devAndreoly.getConteudosConcluidos());
+        System.out.println("XP"+devAndreoly.calcularTotalXp());
+
+        System.out.println("--------------------------");
+
+        Dev devAndre = new Dev();
+        devAndre.setNome("Andre");
+        devAndre.escreverBootcamp(bootCamp);
+        System.out.println("Conteudos Inscritos Andre:"+devAndre.getConteudosInscristos());
+        devAndre.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Andre:"+devAndre.getConteudosInscristos());
+        System.out.println("Conteudos Concluidos Andre:"+devAndre.getConteudosConcluidos());
+        System.out.println("XP"+devAndre.calcularTotalXp());
     }
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,11 +10,25 @@ public class Dev {
     private Set<Conteudo> conteudosInscristos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void escreverBootcamp (BootCamp bootCamp){}
+    public void escreverBootcamp (BootCamp bootCamp){
+        this.conteudosInscristos.addAll(bootCamp.getConteudos());
+        bootCamp.getDevsInscrintos().add(this);                                          //<----Pegando tudo de dentro de conteudos e add em inscritos.
+    }
 
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscristos.stream().findFirst();
+       if(conteudo.isPresent()) {
+         this.conteudosConcluidos.add(conteudo.get());
+         this.conteudosInscristos.remove(conteudo.get());                                //<----Colocando conteudo dos inscritos em comcluido.
+       }else {
+           System.err.println("Você não está matriculado em nenhum conteudo.");
+       }
+    }
 
-    public void calcularTotalXp(){}
+
+    public double calcularTotalXp(){
+        return  this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
 
     public String getNome() {
         return nome;
